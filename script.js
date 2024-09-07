7eddb3825ba57b6db8cd354a5914d9895dfb5b72
Cruz Gallegos <cruzmgallegos11@hotmail.com>
2024-09-06 23:40:26 -0700

    added functions

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-test
+const todos = [];
+
+// Display of the to do list
+function showTodos(){
+    console.clear();
+    console.log('Your To-Do list:');
+
+    if (todos.length === 0){
+        console.log('No to-dos yet');
+    } else {
+
+    }
+}
+
+// Adds stuff to the to do list
+function addTodo(todo){
+
+}
+
+// Delete from the to do list
+function deleteTodo(index){
+
+}
